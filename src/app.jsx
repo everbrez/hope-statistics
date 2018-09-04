a8b9c0b01103ece6243df250aa1124bcbe17b599
everbrez <everbrez@gmail.com>
2018-09-04 22:15:25 +0800

    获取文章数据方法

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,12 @@
-import Dairies from './articles';
+import {PersonList} from './articles';
 jQuery(document).ready(init);
 
-function init() {
+async function init() {
 	const startDate = '2018-08-05',
 		endDate = '2018-09-06';
 
-	const dairys = new Dairies();
-	dairys.getData({
-		startDate,
-		endDate
-	}, data => {
-		console.log(data);
-		$('#app').text(data);
-	})
+	const dairys = new PersonList();
+	const data = await dairys.getData();
+	console.log(data);
 
 }
--- src/articles.js
+++ src/articles.js
@@ -1,28 +1,68 @@
 class Articles {
 	constructor() {
-			this.baseUrl =  'http://ce.sysu.edu.cn/hope/statistics/Index.aspx';
+		this.baseUrl = 'http://ce.sysu.edu.cn/hope/statistics/Index.aspx';
 	}
 
-	getData(options, callback) {
-		const url = `${this.config.url}?id=${this.id}&startDate=${options.startDate}&endDate=${options.endDate}`;
-		jQuery.ajax(url, {
-			complete: xhr => {
-				const data = this.processData(xhr.responseText);
-				callback(data);
-			}
+	async getData(options) {
+		const data = await this.get(this.baseUrl, {
+			id: this.id,
+			type: this.type,
+			...options
+		});
+
+		return this.processData(data);
+	}
+
+	get(url, data) {
+		return new Promise((resolve, reject) => {
+			jQuery.get(url, data)
+				.done(res => resolve(res))
+				.fail(error => reject(error));
 		});
 	}
 
 	processData(data) {
 		const pattern = /(?<=<!--data-start--\>)[\w\W]+(?=\<\!--data-end--\>)/gm;
-		return JSON.parse(pattern.exec(data)[0]).data;
+		const result =  JSON.parse(pattern.exec(data)[0]).data;
+		return result;
 	}
 
 }
 
-class Dairies extends Data {
-	constructor(url) {
-		super(url);
+class Dairies extends Articles {
+	constructor() {
+		super();
 		this.id = 78;
+		this.type = 'dairy';
 	}
+}
+
+class Summaries extends Articles {
+	constructor() {
+		super();
+		this.id = '77,321';
+		this.type = 'summary'
+	}
+}
+
+class Notes extends Articles {
+	constructor() {
+		super();
+		this.id = '19,21,22,28,23,24,25,26,27,179,29';
+		this.type = 'note'
+	}
+}
+
+class PersonList extends Articles {
+	constructor() {
+		super();
+		this.type = 'person'
+	}
+}
+export {
+	Articles,
+	Dairies,
+	Summaries,
+	Notes,
+	PersonList
 }
