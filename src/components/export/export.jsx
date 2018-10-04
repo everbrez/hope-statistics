1de3a8b6cd2e62704ccfacd41a5e93f754f5ffd1
everbrez <everbrez@gmail.com>
2018-10-04 17:50:42 +0800

    [content]init bind & [export]onCopy

--- src/components/export/export.jsx
+++ src/components/export/export.jsx
@@ -1,20 +1,28 @@
-import React, {Component} from 'react';
-import {Button, Notification} from 'element-react';
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import React, { Component } from 'react';
+import { Button, Notification } from 'element-react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 export default class Export extends Component {
-  handleClick() {
-    Notification({
-      title: '导出结果',
-      message: '复制成功',
-      type: 'success'
-    });
+  onCopy(text, res) {
+    if (res) {
+      Notification({
+        title: '导出结果',
+        message: '复制HTML代码成功，请在编辑器中继续操作。',
+        type: 'success'
+      });
+    } else {
+      Notification({
+        title: '导出结果',
+        message: '复制失败',
+        type: 'error'
+      });
+    }
   }
 
   render() {
     return (
-      <CopyToClipboard text={this.props.data}>
-          <Button onClick={this.handleClick} style={{marginTop: 20}} >导出结果</Button>
+      <CopyToClipboard text={this.props.data} onCopy={this.onCopy}>
+        <Button style={{ marginTop: 20 }} >导出结果</Button>
       </CopyToClipboard>
     )
   }
